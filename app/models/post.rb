d48b13ceac9871aeaeca0a036c96512eb3039a5b
jinook929 <jinook929@gmail.com>
2021-03-02 12:43:00 -0500

    set associations among models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user  
+  belongs_to :country
+  has_many :comments
+  has_many :commenters, through: :comments, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-
+  has_many :posts
+  has_many :comments
+  has_many :countries, through: :posts, source: :country
+  has_many :commented_posts, through: :comments, source: :post
 end
